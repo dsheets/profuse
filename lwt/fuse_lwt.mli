37acaf454987f20c6e241cef3433eccb069923ac
David Sheets <sheets@alum.mit.edu>
2016-12-12 10:56:19 +0000

    Ensure Fuse_lwt.Socket is exposed

--- lwt/fuse_lwt.mli
+++ lwt/fuse_lwt.mli
@@ -3,6 +3,8 @@ module type IO_LWT = Fuse.IO with type 'a t = 'a Lwt.t
 
 module type FS_IO_LWT = Fuse.FS_IO with type 'a IO.t = 'a Lwt.t
 
+module Socket : Fuse.SOCKET  with type 'a io = 'a Lwt.t
+
 module IO : IO_LWT
 
 module type FS_LWT = Fuse.FS with module Calls.IO = IO
